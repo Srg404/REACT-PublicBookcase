5cc5d2f654dbb19f9554a616e4de218deb3fdae9
Serge Lecomte <srg404@gmail.com>
2021-10-19 22:44:15 +0200

    create hook for call API

--- public-bookcase/src/components/display-map/display-map.jsx
+++ public-bookcase/src/components/display-map/display-map.jsx
@@ -1,30 +1,25 @@
 import 'leaflet/dist/leaflet.css';
 import './display-map.scss';
 
-import axios from 'axios';
-import { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import { iconCustom } from './icon-custom';
+import { useAxios } from '../../utils/useAxios';
 
 function DisplayMap() {
 
-    const baseURL = 'https://www.odwb.be/explore/dataset/emplacements-des-boites-a-livres-dandenne/download/?format=json&timezone=Europe/Berlin&lang=fr';
-    const [bookCaseList, setBookCaseList] = useState([]);
-    // status authorize values : 'waiting', 'done', 'error'
-    const [status, setStatus] = useState('waiting');
-  
-    useEffect(() => {
-      axios.get(baseURL)
-        .then((response) => {
-          console.log(response.data);
-          setBookCaseList(response.data);
-          setStatus('done');
-        })
-        .catch((error) => {
-          console.log(error);
-          setStatus('error');
-        })
-    }, []);
+    const { status, data } = useAxios('https://www.odwb.be/explore/dataset/emplacements-des-boites-a-livres-dandenne/download/?format=json&timezone=Europe/Berlin&lang=fr')
+
+    if (status === 'waiting') return (
+        <div className="display-map">
+            <p>Chargement...</p>
+        </div>
+    );
+
+    if (status === 'error') return (
+        <div className="display-map">
+            <p>Une erreur malencontrueuse c'est produit veuillez recharger la page ! </p>
+        </div>
+    );
 
     return (
         <div className="display-map">
@@ -32,7 +27,7 @@ function DisplayMap() {
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {bookCaseList.map((bookCase) => (
+          {data.map((bookCase) => (
             <Marker
               key={bookCase.recordid}
               position={bookCase.fields.geo_point_2d}
---
+++ public-bookcase/src/utils/useAxios.jsx
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+export function useAxios(baseURL) {
+
+    const [data, setData] = useState([]);
+    const [status, setStatus] = useState('waiting');
+    // status authorize values : 'waiting', 'done', 'error'
+
+    useEffect(() => {
+        axios.get(baseURL)
+          .then((response) => {
+            console.log(response.data);
+            setData(response.data);
+            setStatus('done');
+          })
+          .catch((error) => {
+            console.log(error);
+            setStatus('error');
+          })
+      }, [baseURL]);
+
+    return {status, data}
+}

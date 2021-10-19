ede8ad1e9d89b103b66fd20edb11bccbe819c57e
Serge Lecomte <srg404@gmail.com>
2021-10-19 17:30:31 +0200

    Add map example with react-leaflet

--- public-bookcase/src/pages/map/map.jsx
+++ public-bookcase/src/pages/map/map.jsx
@@ -1,7 +1,18 @@
-import Titles from '../../components/titles/titles';
 import './map.scss';
+import pinImg from '../../assets/images/pin.svg';
+import Titles from '../../components/titles/titles';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 
 function MapPage() {
+
+  const iconPerson = new L.Icon({
+    iconUrl: pinImg,
+    iconSize: [32, 40],
+    iconAnchor: [16, 40],
+  });
+
   return (
     <div id="map" className="map">
       <Titles
@@ -12,7 +23,17 @@ function MapPage() {
         [Sidebar]
       </div>
       <div className="map-display">
-        [Map]
+        <MapContainer center={[50.489, 5.095]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker 
+            position={[50.489, 5.095]}
+            icon= { iconPerson }
+            >
+          </Marker>
+        </MapContainer>
       </div>
     </div>
   );

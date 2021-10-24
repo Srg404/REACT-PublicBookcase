9a466177a4752024b52d3ad30091f8db0369656e
Serge Lecomte <srg404@gmail.com>
2021-10-24 19:53:12 +0200

    Add Parse initialization configuration

--- public-bookcase/src/App.js
+++ public-bookcase/src/App.js
@@ -5,6 +5,14 @@ import AboutPage from './pages/about/about';
 import Navigation from './components/navigation/navigation';
 import ErrorPage from './pages/error/error';
 import MapPage from './pages/map/map';
+import Parse from 'parse/dist/parse.min.js';
+import { parseApplicationId, parseJavascriptId  } from './utils/keys'
+
+const PARSE_APPLICATION_ID = parseApplicationId;
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = parseJavascriptId;
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 function App() {
   return (
